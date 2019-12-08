3c3fa7667d397f78c90630a5f61d99e9a279e8f9
gaozhichao <gaozhichao@wanda.cn>
2019-12-08 19:41:24 +0800

    初始化

---
+++ angel-js/docs/.vuepress/config.js
@@ -0,0 +1,178 @@
+module.exports = {
+  base: '/angel-js/',
+  title: '深入理解 angel-js',
+  description: 'angel-js 中文文档',
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: [
+    '@vuepress/pwa',
+    '@vuepress/back-to-top',
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-106861408-1' // UA-00000000-0
+      }
+    ]
+  ],
+  // theme: [],
+  themeConfig: {
+    repo: 'jkchao/angel-js',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '编辑此页',
+    activeHeaderLinks: false,
+    sidebarDepth: 3,
+    lastUpdated: '上次更新',
+    // algolia: {
+    //   apiKey: 'fd0efd57c48824ceb1bcfa9690dba5b0',
+    //   indexName: 'jkchao_typescript'
+    // },
+    adsConfig: [
+      { title: '与我交流', src: '/angel-js/contact.png' },
+      {
+        title: '购买此书',
+        src:
+          'https://static.jkchao.cn/2019-11-22/WechatIMG719.png?imageMogr2/auto-orient/thumbnail/80x/blur/1x0/quality/75|imageslim',
+        url: 'https://item.jd.com/12755624.html'
+      }
+    ],
+    nav: [
+      // { text: '原书链接', link: 'https://basarat.gitbooks.io/typescript/content/docs/getting-started.html' },
+      // { text: 'Blog', link: 'https://jkchao.cn' }
+    ],
+    sidebar: [
+      {
+        title: '写在前面',
+        collapsable: false,
+        children: ['/']
+      },
+      {
+        title: 'Api',
+        collapsable: false,
+        children: [
+          '/project/compilationContext',
+          // '/project/declarationspaces',
+          // '/project/modules',
+          // '/project/namespaces',
+          // '/project/dynamicImportExpressions'
+        ]
+      },
+      // {
+      //   title: 'TypeScript 类型系统',
+      //   collapsable: false,
+      //   children: [
+      //     '/typings/overview',
+      //     '/typings/migrating',
+      //     '/typings/types',
+      //     '/typings/ambient',
+      //     '/typings/interfaces',
+      //     '/typings/enums',
+      //     '/typings/lib',
+      //     '/typings/functions',
+      //     '/typings/callable',
+      //     '/typings/typeAssertion',
+      //     '/typings/freshness',
+      //     '/typings/typeGuard',
+      //     '/typings/literals',
+      //     '/typings/readonly',
+      //     '/typings/generices',
+      //     '/typings/typeInference',
+      //     '/typings/typeCompatibility',
+      //     '/typings/neverType',
+      //     '/typings/discrominatedUnion',
+      //     '/typings/indexSignatures',
+      //     '/typings/movingTypes',
+      //     '/typings/exceptionsHanding',
+      //     '/typings/mixins',
+      //     '/typings/thisType'
+      //   ]
+      // },
+      // {
+      //   title: 'JSX',
+      //   collapsable: false,
+      //   children: ['/jsx/support', '/jsx/reactJSX', '/jsx/nonReactJSX']
+      // },
+      // {
+      //   title: 'TypeScript 错误提示',
+      //   collapsable: false,
+      //   children: ['/error/interpreting', '/error/common']
+      // },
+      // {
+      //   title: 'TIPs',
+      //   collapsable: false,
+      //   children: [
+      //     '/tips/stringBasedEmuns',
+      //     '/tips/nominalTyping',
+      //     '/tips/statefulFunctions',
+      //     '/tips/bind',
+      //     '/tips/curry',
+      //     '/tips/typeInstantiation',
+      //     '/tips/lazyObjectLiteralInitialization',
+      //     '/tips/classAreUseful',
+      //     '/tips/avoidExportDefault',
+      //     '/tips/limitPropertySetters',
+      //     '/tips/createArrays',
+      //     '/tips/outFileCaution',
+      //     '/tips/staticConstructors',
+      //     '/tips/singletonPatern',
+      //     '/tips/functionParameters',
+      //     '/tips/truthy',
+      //     '/tips/buildToggles',
+      //     '/tips/typesafeEventEmitter',
+      //     '/tips/metadata',
+      //     '/tips/covarianceAndContravariance',
+      //     '/tips/infer'
+      //   ]
+      // },
+      // {
+      //   title: 'TypeScript 编译原理',
+      //   collapsable: false,
+      //   children: [
+      //     '/compiler/overview',
+      //     '/compiler/program',
+      //     '/compiler/ast',
+      //     '/compiler/scanner',
+      //     '/compiler/parser',
+      //     '/compiler/binder',
+      //     '/compiler/checker',
+      //     '/compiler/emitter'
+      //   ]
+      // },
+      // {
+      //   title: 'TypeScript FAQs',
+      //   collapsable: false,
+      //   children: [
+      //     './faqs/common-bug-not-bugs',
+      //     './faqs/common-feature-request',
+      //     './faqs/type-system-behavior',
+      //     './faqs/function',
+      //     './faqs/class',
+      //     './faqs/generics',
+      //     './faqs/modules',
+      //     './faqs/enums',
+      //     './faqs/type-guards',
+      //     './faqs/jsx-and-react',
+      //     './faqs/thing-that-dont-work',
+      //     './faqs/commandline-behavior',
+      //     './faqs/tsconfig-behavior'
+      //   ]
+      // },
+      {
+        title: 'TypeScript 更新',
+        collapsable: false,
+        children: ['/new/typescript-3.7']
+      }
+      // {
+      //   title: 'TypeScript 更新',
+      //   collapsable: false,
+      //   children: ['/new/typescript-3.7']
+      // }
+    ]
+  }
+};
---
+++ angel-js/docs/README.md
@@ -0,0 +1,84 @@
+# 深入理解 angel-js
+
+此书是 [《TypeScript Deep Dive》](https://github.com/basarat/typescript-book/) 的中文翻译版，感谢作者 [Basarat](https://github.com/basarat) 的付出。
+
+## Why
+
+<img :src="$withBase('/typescript-downloads.jpg')" alt="ide"/>
+
+> 数据来源：[npm 包下载量](https://npm-stat.com/charts.html?package=typescript&from=2016-01-01&to=2018-07-31)
+
+如你所见，TypeScript 发展至今，已经成为大型项目的标配，其提供的静态类型系统，大大增强了代码的可读性以及可维护性；同时，它提供最新和不断发展的 JavaScript 特性，能让我们建立更健壮的组件。
+
+[《TypeScript Deep Dive》](https://github.com/basarat/typescript-book/) 是一本很好的开源书，从基础到深入，很全面的阐述了 TypeScript 的各种魔法，不管你是新手，还是老鸟，它都将适应你。此外，它不同于 TypeScript 官方给出的文档（当然 TypeScript 给出的文档是很好的），在此书中，结合实际应用下的场景用例，你将能更深入的理解 TypeScript。
+
+如今社区已经存在部分翻译，但都似乎已经停止更新。
+
+于是在某天的某个冲动之下，这个 RP 就诞生了。
+
+## 翻译内容
+
+《TypeScript Deep Dive》 书中包含一部分 JavaScript Future 和一些其他的内容，在这里，我们并不打算翻译它，如果你有兴趣，可以查看原书中 [JavaScript Future](https://basarat.gitbooks.io/typescript/content/docs/future-javascript.html) 的有关章节。
+
+由于 TypeScript 更新频繁，在此书中，我也将加入一些原书中并没有涉及到的知识点，希望和大家相互学习，一起进步。
+
+此外，在不违背原作者本意前提下，为了更直观的表达，部分内容将采用意译，而非直译。
+
+## How to contribute
+
+你可以：
+
+- 通过 PR 修改错别字，或者错误的格式；
+- 发 issue 讨论文章中出现的一些不合理地方；
+- 翻译 TODO 文件夹下的文章，并顺手 Email 我。
+
+希望你在翻译或者 PR 之前，阅读[中文文章排版指北](https://github.com/mzlogin/chinese-copywriting-guidelines)。
+
+## 最后
+
+如果你和我一样对 TypeScript 充满兴趣，可以订阅（star）本项目，及时收到有关于此项目的更新。
+
+如果你对文章有任何疑问，欢迎提交 [issues](https://github.com/jkchao/angel-js/issues) 和我交流。
+
+如果你认为有些地方翻译不够准确，或者你想补充一些文中没提到但是非常有意思的知识点，欢迎 [PR](https://github.com/jkchao/angel-js/pulls)。
+
+赞赏支持：
+
+<style>
+.appreciate {
+  display: flex;
+  justify-content: space-around;
+}
+
+.appreciate-item {
+  text-align: center;
+}
+
+@media (max-width: 719px) {
+  .appreciate {
+    flex-wrap: wrap;
+  }
+
+  .appreciate-item {
+    width: 100%;
+  }
+}
+
+
+</style>
+
+<div class="appreciate">
+  <div class="appreciate-item">
+    <img :src="$withBase('/wechat.jpg')" width="200" alt="微信">
+    <p>微信</p>
+  </div>
+
+  <div class="appreciate-item">
+    <img :src="$withBase('/zhifubao.jpg')" width="200" alt="支付宝">
+    <p>支付宝</p>
+  </div>
+</div>
+
+如果你喜欢纸质书籍，可以通过[京东](https://item.jd.com/12755624.html)或者[当当](http://product.m.dangdang.com/28487648.html?t=1574581821)，来购买此书。
+
+![图书封面](https://static.jkchao.cn/2019-11-22/WechatIMG719.png?imageMogr2/auto-orient/thumbnail/360x/blur/1x0/quality/75|imageslim)
